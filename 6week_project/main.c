c3b7a62a59048b56d50df9350485aa3e3cda5259
Ayaanjung00 <mimayjun@naver.com>
2022-10-12 00:11:39 +0900

    6week_project RSP random game

--- 6week_project/main.c
+++ 6week_project/main.c
@@ -4,19 +4,97 @@
 
 #include <time.h>   // time()함수를 포함하는 라이브러리
 
+#include <string.h>	// strcmp()함수를 포함하는 라이브러리
+
+int win = 0;
+int draw = 0;
+
+int game(int player, int ai);
 
 int main(void) {
 
-	srand(time(NULL)); // 매번 다른 난수가 발생하도록 하기 위해서 사용
+	char sel[][7] = { NULL };
+	srand(time(NULL));
+	char rsp[3][7] = { "바위", "가위", "보" };
+	
+	while(1)
+	{
+		int ai, player = 0;
+		ai = (rand() % 3 + 1);
 
-	int random = 0;
+		printf("선택하시오\n1. 바위 2. 가위, 3. 보 : ");
+		scanf_s("%s", &sel, 7);
 
-	for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 3; i++) {
+			int num = strcmp(sel[0], rsp[i]);
+			if (num == 0) {
+				player = i + 1;
+			}
 
-		random = rand() % 3;          // 가위, 바위, 보를 위한 3개의 난수 생성
-
-		printf("%d\n", random);
-
+		}
+		printf("나의 선택은 %d번 '%s'입니다.\n", player, sel);
+		if (game(player, ai) == 0) break;
 	}
-
+	printf("게임의 결과 : %d승 %d무", win, draw);
 }
+
+int game(int player, int ai) 
+{
+	if (player == 1) {
+		printf("당신은 바위를 선택, ");
+		if (ai == 1) {
+			printf("컴퓨터는 바위를 선택, ");
+			printf("비겼습니다.\n");
+			draw++;
+		}
+		else if (ai == 2) {
+			printf("컴퓨터는 가위를 선택, ");
+			printf("이겼습니다.\n");
+			win++;
+		}
+		else if (ai == 3) {
+			printf("컴퓨터는 보를 선택, ");
+			printf("졌습니다.\n");
+			return 0;
+		}
+	}
+	if (player == 2) {
+		printf("당신은 가위를 선택, ");
+		if (ai == 1) {
+			printf("컴퓨터는 바위를 선택, ");
+			printf("졌습니다.\n");
+			return 0;
+		}
+		else if (ai == 2) {
+			printf("컴퓨터는 가위를 선택, ");
+			printf("비겼습니다.\n");
+			draw++;
+		}
+		else if (ai == 3) {
+			printf("컴퓨터는 보를 선택, ");
+			printf("이겼습니다.\n");
+			win++;
+		}
+	}
+	if (player == 3) {
+		printf("당신은 보를 선택, ");
+		if (ai == 1) {
+			printf("컴퓨터는 바위를 선택, ");
+			printf("이겼습니다.\n");
+			win++;
+		}
+		else if (ai == 2) {
+			printf("컴퓨터는 가위를 선택, ");
+			printf("졌습니다.\n");
+			return 0;
+		}
+		else if (ai == 3) {
+			printf("컴퓨터는 보를 선택, ");
+			printf("비겼습니다.\n");
+			draw++;
+		}
+	}	
+}
+
+
+
